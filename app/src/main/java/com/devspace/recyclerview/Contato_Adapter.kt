2937d1e33810e036857e89b67e9f61940c5ef00e
calculo-combustivel <miguelingoschaade@gmail.com>
2024-08-29 21:32:29 -0300

    app

--- app/src/main/java/com/devspace/recyclerview/Contato_Adapter.kt
+++ app/src/main/java/com/devspace/recyclerview/Contato_Adapter.kt
@@ -1,4 +1,54 @@
 package com.devspace.recyclerview
 
-class Contato_Adapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class Contato_Adapter :
+    ListAdapter <class_contato,Contato_Adapter.ContactViewHolder>(ContactDifuilts()){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list,parent,false)
+        return ContactViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+
+        val contact = getItem(position)
+        holder.bind(contact)
+
+    }
+
+    class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val tvName = view.findViewById<TextView>(R.id.tv_name)
+        private val tvPhone = view.findViewById<TextView>(R.id.tv_phone)
+        private val image = view.findViewById<ImageView>(R.id.image)
+
+        fun bind(contact:class_contato){
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
+            image.setImageResource(contact.icno)
+        }
+
+    }
+
+    class ContactDifuilts : DiffUtil.ItemCallback<class_contato>() {
+        override fun areItemsTheSame(oldItem: class_contato, newItem: class_contato): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: class_contato, newItem: class_contato): Boolean {
+            return oldItem.name == newItem.name
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        val adapter = Contato_Adapter()
+
+        rvList.adapter=adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+        adapter.submitList(classContato)
 
     }
 }
